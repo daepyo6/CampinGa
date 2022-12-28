3765ce6817bd17bc6be4e807a546e7c487885ec7
JAVA01 <JAVA01@DESKTOP-T35QNKU>
2022-12-28 13:51:57 +0900

    test

--- CampinGa/src/main/webapp/member/script/member.js
+++ CampinGa/src/main/webapp/member/script/member.js
@@ -1,1 +1,16 @@
 // member JavaScript 
+
+
+function loginCheck(){
+	if(document.loginFrm.id.value==""){
+		alert("아이디는 필수입력사항입니다");
+		document.loginFrm.id.focus();
+		return false;
+	}else if(document.loginFrm.pwd.value==""){
+		alert("비밀번호는 필수입력사항입니다");
+		document.loginFrm.pwd.focus();
+		return false;
+	}else{
+		return true;	
+	}	
+}
